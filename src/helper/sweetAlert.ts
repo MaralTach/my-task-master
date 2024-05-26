7d3c39a81e39815ec14b29b8d5d894ddcc627277
MaralTach <yoloten.maral@gmail.com>
2024-05-26 19:59:25 +0300

    update

--- src/helper/sweetAlert.ts
+++ src/helper/sweetAlert.ts
@@ -1,1 +1,14 @@
-export {};
+import Swal from "sweetalert2"
+
+export enum SweetIcon {
+    SUCCESS = "success",    
+
+}
+
+export const notify = (msg: string) =>Swal.fire({ 
+    title:"Success",
+    icon:"success",
+    timer:2000
+    
+})
+
